42cc06ce5b2cc6f41df2d699f94f9bb95e6bef91
André Oliveira <132096135+AFCOliveira@users.noreply.github.com>
2023-10-31 21:44:53 -0300

    config cookie

--- src/pages/api/users/index.api.ts
+++ src/pages/api/users/index.api.ts
@@ -1,5 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import { prisma } from '../../../lib/prisma'
+import { setCookie } from 'nookies'
 
 export default async function handler(
   req: NextApiRequest,
@@ -9,13 +10,31 @@ export default async function handler(
     return res.status(405).end()
   }
 
-  const { name, usernames } = req.body
+  const { name, username } = req.body
+
+  const userExists = await prisma.user.findUnique({
+    where: {
+      username,
+    },
+  })
+
+  if (userExists) {
+    return res.status(400).json({
+      message: 'User already exists.',
+    })
+  }
 
   const user = await prisma.user.create({
     data: {
       name,
-      usernames,
+      username,
     },
   })
+
+  setCookie({ res }, '@ignitecall:userId', user.id, {
+    maxAge: 60 * 60 * 24 * 7, // 7 days
+    path: '/',
+  })
+
   return res.status(201).json(user)
 }
